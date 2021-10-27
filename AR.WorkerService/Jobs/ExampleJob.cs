0bbc7f17593400067102aa27a99707ee4b0a0ad0
Antonio Gonzalez Restrepo <angonzalez@sistecredito.com>
2021-10-27 15:11:34 -0500

    Initial Commit

---
+++ AR.WorkerService/Jobs/ExampleJob.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace AR.WorkerService.Jobs
+{
+    public class ExampleJob : IJob
+    {
+        private readonly ILogger<ExampleJob> _logger;
+
+        public ExampleJob(ILogger<ExampleJob> logger)
+        {
+            _logger = logger;
+        }
+ 
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("{jobName} running at {time}", this.GetType().Name, DateTimeOffset.Now);
+            return Task.CompletedTask;
+        }
+    }
+}
