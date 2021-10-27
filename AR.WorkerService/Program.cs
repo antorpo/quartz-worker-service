8f4ff382685a25ef713712dd9ebfa98a5b96da00
Antonio Gonzalez Restrepo <angonzalez@sistecredito.com>
2021-10-27 18:01:27 -0500

    Added Serilog to replace the default ASP.NET Core Logging

--- AR.WorkerService/Program.cs
+++ AR.WorkerService/Program.cs
@@ -1,7 +1,9 @@
 using AR.WorkerService.Configuration;
 using AR.WorkerService.Jobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Quartz;
+using Serilog;
 
 namespace AR.WorkerService
 {
@@ -15,11 +17,22 @@ namespace AR.WorkerService
         // https://www.fixedbuffer.com/worker-service-como-crear-un-servicio-net-core-3-multiplataforma/
         public static IHostBuilder CreateHostBuilder(string[] args) {
 
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("config/appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
             return Host.CreateDefaultBuilder(args)
                 .UseWindowsService(options => { // Install service in Windows
                     options.ServiceName = "ExampleServiceName";
                 }) 
                 .UseSystemd() // Install service in Linux
+                .UseSerilog((context, configuration) =>
+                
+                    // Replace the default ASP.NET Core logging system to use Serilog
+                    configuration.ReadFrom.Configuration(config)
+                        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName))
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddQuartz(q => {
